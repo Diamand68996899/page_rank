253126beecc37c84216c6624d7003d80c46931ae
Diamand68996899 <diamand68996899@hotmail.com>
2022-07-08 20:15:32 +0200

    Page Rank: project 1/2 AI course
    # summary: project2_1 to rank page importance according to citation by AI (Markov Chain)
    
    original file without implementation of
    - transition_model
    - sample_pagerank
    -iterate_pagerank
    
    - Corpus0

--- pagerank/pagerank.py
+++ pagerank/pagerank.py
@@ -17,20 +17,15 @@ def main():
     ##    sys.exit("Usage: python pagerank.py corpus")
     corpus = crawl(goal_dir)   ## sys.argv[1]  replaced
     ranks = sample_pagerank(corpus, DAMPING, SAMPLES)   # page: rank
-    ## corpus = {'1.html': {'2.html'}, '2.html': {'1.html', '3.html'}, '3.html': {'4.html', '2.html'}, '4.html': {'2.html'}}
     print(f"PageRank Results from Sampling (n = {SAMPLES})")
-    ###
-
-    ##for page in sorted(ranks):
-      ##  print(f"  {page}: {ranks[page]:.4f}")
-    ## ranks = iterate_pagerank(corpus, DAMPING)
-    ##print(f"PageRank Results from Iteration")
 
     for page in sorted(ranks):
-        #print('____________>',page, sorted(ranks))
         print(f"  {page}: {ranks[page]:.4f}")
-        #print(page)   ## to be deleted
+    ranks = iterate_pagerank(corpus, DAMPING)
+    print(f"PageRank Results from Iteration")
 
+    for page in sorted(ranks):
+        print(f"  {page}: {ranks[page]:.4f}")
 
 def crawl(directory):
     """
@@ -100,27 +95,30 @@ def sample_pagerank(corpus, damping_factor, n):
         return random_page
 
     # transit state
-    page_prob = {}
-    num_chain = 5
+    first_page_prob = {}
+    num_chain = 6
     parent_pages = {}
     parent_pages_probability = {}
-    current_page = ''
     page_sample ={}
+
+    # initialization of the very first sample
+
 
     for page in corpus:
         page_sample[page] = []
         parent_pages[page] = set()
+        first_page_prob[page] = 1/ len(corpus)
 
-    for j in range(SAMPLES):
 
+    pr_value = {}
+    pr_value_corr = {}
+
+    for k in range(SAMPLES):
+
+        current_page = random_page(corpus, first_page_prob)  # generating random page by prob for first sample
+        # generat chain current-kid (parent-current)
         for i in range(num_chain):
-            if len(page_prob) == 0:    #   initialization first random page, start
-                for page in corpus:
-                    page_prob[page] = 1/len(corpus)
-                random_current_page = random_page(corpus, page_prob)  # generating random page by prob
-                current_page = random_current_page
-
-            # next_page by calling transition_model return {next_page:prob}
+            # surfing on current page and jump to kid page by current_page_prob
             current_page_prob = transition_model(corpus, current_page, damping_factor)  # surfing current page
             kid_page = random_page(corpus, current_page_prob)   # link to kid page from
             parent_pages[kid_page].add(current_page)  # generating current page as parent to kid page
@@ -130,37 +128,97 @@ def sample_pagerank(corpus, damping_factor, n):
         for page in parent_pages_probability:
             page_sample[page].append(parent_pages_probability[page])
 
-
-    pr_value = {}
     for page in corpus:
-        print(page, numpy.average(page_sample[page]))
         pr_value[page] = numpy.average(page_sample[page])
-        print(page, parent_pages[page])
 
-    # correct with algorithm
-    print(pr_value)
-
-    pr_value_corr = {}
     for page in pr_value:
-        pr_value_corr[page] = (1-damping_factor)/len(corpus)
+        pr_value_corr[page] = (1 - damping_factor) / len(corpus)
         for p_page in parent_pages[page]:
-            if p_page != page:
-                num_links = len(corpus[p_page])
-                pr_value_corr[page] = pr_value_corr[page] + damping_factor*(pr_value[p_page]/num_links)
+                if p_page != page:
+                    num_links = len(corpus[p_page])
+                    pr_value_corr[page] = float(pr_value_corr[page] + damping_factor * (pr_value[p_page] / num_links))
 
-    print(pr_value_corr)
-    return pr_value
+
+    return pr_value_corr
 
 def iterate_pagerank(corpus, damping_factor):
-    """
-    Return PageRank values for each page by iteratively updating
-    PageRank values until convergence.
 
-    Return a dictionary where keys are page names, and values are
-    their estimated PageRank value (a value between 0 and 1). All
-    PageRank values should sum to 1.
-    """
-    raise NotImplementedError
+    pr_value0 ={}
+    pr_value ={}
+    source_pages = {}
+    num_links = {}
+    for page in corpus:
+        source_pages[page] = set()
+        num_links[page] = len(corpus[page])
+
+    for page in corpus:
+        for link in corpus[page]:
+            source_pages[link].add(page)
+
+    # initialization of start point for pr_value[page]
+    num_linked_pages = {}
+    total_linked_pages=0
+    for page in source_pages:
+        num_linked_pages[page] = len(source_pages[page])
+        total_linked_pages = total_linked_pages + len(source_pages[page])
+
+    # pr_delta record all the changes of pr values  per page during iteration
+    pr_delta = {}
+    for page in corpus:
+        pr_value0[page]= num_linked_pages[page]/total_linked_pages
+        pr_delta[page] = 0
+
+    flag = True
+    delta = 0
+
+    while flag:
+
+        weight = []
+        population = []
+        for page in corpus:
+            population.append(page)
+            weight.append(100 * pr_value0[page])
+
+        surf_page = random.choices(population, weight, k=1)
+        page_prob = transition_model(corpus, surf_page[0], damping_factor)
+
+        # change of pr_value0
+        for page in corpus:
+            pr_delta[page] = pr_value0[surf_page[0]]*page_prob[page]+pr_delta[page]
+            delta = delta + pr_delta[page]
+
+        # implement changes in pr_value
+        pr_delta_inter = {}
+        normal = 0
+        for page in corpus:
+            pr_delta_inter[page] = pr_delta[page]/(1+delta)
+            pr_value[page] = (pr_value0[page]+pr_delta_inter[page])
+            normal = pr_value[page] + normal
+        pr_value_i = {}
+        for page in corpus:
+            pr_value_i[page] = pr_value[page]/normal
+
+        # new pr value according to algorithom
+        for page in corpus:
+            pr_value[page] = 0
+
+            for linked_page in source_pages[page]:
+                pr_value[page] =pr_value_i[linked_page]/num_links[linked_page]+pr_value[page]
+
+            pr_value[page] = (1 - damping_factor) / len(corpus)+ damping_factor*(pr_value[page])
+
+        # check the change
+        check = 0
+        for page in corpus:
+            check = check + abs(pr_value[page] - pr_value0[page])
+
+        if check < 0.001:
+            flag = False
+        else:
+            for page in corpus:
+                pr_value0[page] = pr_value[page]
+
+    return pr_value
 
 
 if __name__ == "__main__":
